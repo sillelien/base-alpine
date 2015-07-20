91d5e30306150866492c3e8bbcbdd094a94ec4bc
neilellis <hello@neilellis.me>
2015-07-20 15:51:56 +0100

    Debug and DNS fixes

--- rootfs/bin/dns_hack.sh
+++ rootfs/bin/dns_hack.sh
@@ -6,17 +6,21 @@ do
   link=$(echo ${env_var%_NAME}  | tr '_' '-' | tr '[:upper:]' '[:lower:]')
   domain=$(cat /etc/dnsmasq-resolv.conf | grep search | cut -d' ' -f2)
   nameserver=$(cat /etc/dnsmasq-resolv.conf | grep nameserver | head -1 | cut -d' ' -f2)
-  if nslookup "${link}.${domain}" ${nameserver}
+  if ping -c 1 "${link}.${domain}" &> /dev/null
+  then
+      ip=$(ping -c 1 "${link}.${domain}"| head -1 | cut -d'(' -f2 |  cut -d')' -f1)
+  elif nslookup "${link}.${domain}" ${nameserver}
   then
       ip=$( nslookup "${link}.${domain}" ${nameserver}  | grep Address | tail -1 | cut -d: -f2  | cut -d' ' -f2 2>/dev/null)
-      if [ -n "$ip" ]
-      then
-        echo "${ip} ${link}" >> /tmp/hosts
-      else
-        echo "ip ${link}.${domain} skipped, it didn't resolve second time." 1>&2
-      fi
   else
        echo "ip ${link}.${domain} skipped, it didn't resolve." 1>&2
+      ip=
+  fi
+  if [ -n "$ip" ]
+  then
+    echo "${ip} ${link}" >> /tmp/hosts
+  else
+    echo "ip ${link}.${domain} skipped, it didn't resolve second time." 1>&2
   fi
 
 done
