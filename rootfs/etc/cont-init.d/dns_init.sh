81f167285c7fb2600cadc756999d2ede9afab72a
neilellis <hello@neilellis.me>
2015-07-23 09:54:56 +0100

    Initial /etc/hosts configuration

--- rootfs/etc/cont-init.d/dns_init.sh
+++ rootfs/etc/cont-init.d/dns_init.sh
@@ -1,12 +1,8 @@
-#!/usr/bin/with-contenv sh
-exit 0
-
-
-
-
-
-echo "DNS hacks, first run."
-
+#!/usr/bin/with-contenv ash
+set -x
+echo "DNS hacks, initial hosts generation."
+cp /etc/hosts /etc/hosts.orig
+cp /etc/hosts /tmp/hosts
 if ! ( cat /etc/resolv.conf | grep "nameserver 127.0.0.1" )
 then
     cp -f /etc/resolv.conf /etc/dnsmasq-resolv.conf
@@ -19,6 +15,17 @@ cat /etc/dnsmasq-resolv.conf
 echo
 echo
 
-. /bin/dns_hack.sh
 
-cp -f /tmp/hosts /etc/hosts.links
+env_vars=$(env | grep ".*_PORT_.*_TCP_ADDR=" | cut -d= -f1 | tr '\n' ' ' )
+echo "#Auto Generated - DO NOT CHANGE" >> /tmp/hosts
+for env_var in $env_vars
+do
+  host=$(echo $env_var | awk -F"_PORT_" '{print $1;}' | tr '_' '-' | tr '[:upper:]' '[:lower:]' )
+  ip=$(eval "echo \$$env_var")
+  echo "${ip} ${host}" >> /tmp/hosts
+done
+
+sort -u < /tmp/hosts > /etc/hosts
+
+echo "Initial DNS calculated"
+cat /etc/hosts
