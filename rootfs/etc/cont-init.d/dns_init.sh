0f67e414b56372d87d5b5c209fb84283bf9827ae
neilellis <hello@neilellis.me>
2015-07-31 16:49:06 +0100

    Fixes for Tutum containers that don't have any exposed ports.

--- rootfs/etc/cont-init.d/dns_init.sh
+++ rootfs/etc/cont-init.d/dns_init.sh
@@ -14,15 +14,28 @@ cat /etc/dnsmasq-resolv.conf
 echo
 echo
 
-
-env_vars=$(env | grep ".*_PORT_.*_TCP_ADDR=" | cut -d= -f1 | tr '\n' ' ' )
-echo "#Auto Generated - DO NOT CHANGE" >> /tmp/hosts
-for env_var in $env_vars
-do
-  host=$(echo $env_var | awk -F"_PORT_" '{print $1;}' | tr '_' '-' | tr '[:upper:]' '[:lower:]' )
-  ip=$(eval "echo \$$env_var")
-  echo "${ip} ${host}" >> /tmp/hosts
-done
+if env | grep "_ENV_TUTUM_CONTAINER_FQDN"
+then
+    echo "We're running on Tutum"
+    env_vars=$(env | grep "_ENV_TUTUM_CONTAINER_FQDN=" | cut -d= -f1 | tr '\n' ' ' )
+    echo "#Auto Generated - DO NOT CHANGE" >> /tmp/hosts
+    for env_var in $env_vars
+    do
+      host=$(echo $env_var | awk -F"_ENV_TUTUM_CONTAINER_FQDN" '{print $1;}' | tr '_' '-' | tr '[:upper:]' '[:lower:]' )
+      ip=$( nslookup $(eval "echo \$$env_var") ${nameserver}  | grep Address | tail -1 | cut -d: -f2  | cut -d' ' -f2 2>/dev/null)
+      echo "${ip} ${host}" >> /tmp/hosts
+    done
+else
+    echo "We're not running on Tutum"
+    env_vars=$(env | grep ".*_PORT_.*_TCP_ADDR=" | cut -d= -f1 | tr '\n' ' ' )
+    echo "#Auto Generated - DO NOT CHANGE" >> /tmp/hosts
+    for env_var in $env_vars
+    do
+      host=$(echo $env_var | awk -F"_PORT_" '{print $1;}' | tr '_' '-' | tr '[:upper:]' '[:lower:]' )
+      ip=$(eval "echo \$$env_var")
+      echo "${ip} ${host}" >> /tmp/hosts
+    done
+fi
 
 sort -u < /tmp/hosts > /etc/hosts
 
