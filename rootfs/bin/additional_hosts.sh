625bc5c3e8a26a2f6c3df2112c78158e4d1b257a
neilellis <hello@neilellis.me>
2015-09-04 00:04:56 +0100

    Additional hosts

--- rootfs/bin/additional_hosts.sh
+++ rootfs/bin/additional_hosts.sh
@@ -10,10 +10,11 @@ then
     do
           for suffix in $service1 $service2 $cont1 $cont2
           do
-              if nslookup "${host}.${suffix}" ${nameserver}
+              if nslookup "${host}.${suffix}" ${nameserver} &> /dev/null
               then
                 ip=$( nslookup "${host}.${suffix}" ${nameserver}  | grep Address | tail -1 | cut -d: -f2  | cut -d' ' -f2 2>/dev/null)
                 echo "${ip} ${host}.${suffix}" >> /tmp/hosts
+                echo "Added additional host ${host}.${suffix}=${ip}"
               fi
           done
     done
