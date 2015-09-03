c50c11227f9359a190cb8b8545e50e59acc5265f
neilellis <hello@neilellis.me>
2015-09-04 00:17:33 +0100

    Additional hosts

--- rootfs/bin/additional_hosts.sh
+++ rootfs/bin/additional_hosts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env ash
-nameserver=$(cat /etc/dnsmasq-resolv.conf | grep nameserver | head -1 | cut -d' ' -f2)
+
 if [ -n "$BA_ADDITIONAL_HOSTS" ] && [ -n "$TUTUM_SERVICE_FQDN" ]
 then
     service1=$(echo $TUTUM_SERVICE_FQDN | cut -d'.' -f2-)
@@ -12,7 +12,7 @@ then
           do
               if nslookup -timeout=2 "${host}.${suffix}" 8.8.8.8
               then
-                ip=$( nslookup "${host}.${suffix}" ${nameserver}  | grep Address | tail -1 | cut -d: -f2  | cut -d' ' -f2 2>/dev/null)
+                ip=$( nslookup "${host}.${suffix}" 8.8.8.8  | grep Address | tail -1 | cut -d: -f2  | cut -d' ' -f2 2>/dev/null)
                 echo "${ip} ${host}.${suffix}" >> /tmp/hosts
                 echo "Added additional host ${host}.${suffix}=${ip}"
               fi
