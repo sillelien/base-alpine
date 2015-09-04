6f99bbe085909e2f442353cacb84b0aa47fae2fb
Neil Ellis <hello@neilellis.me>
2015-09-04 18:25:30 +0100

    DNS fixes

--- rootfs/bin/get_hosts_from_tutum.sh
+++ rootfs/bin/get_hosts_from_tutum.sh
@@ -1,8 +1,8 @@
 parseServiceLinks() {
-    while read ip service_link
+    while read ip service_link host
     do
-        host=$(curl -s -H "Authorization: $TUTUM_AUTH" -H "Accept: application/json" ${TUTUM_REST_HOST}${service_link} | jq -r '.public_dns')
-        echo "${ip} ${host}" >> /tmp/hosts
+        fqdn=$(curl -s -H "Authorization: $TUTUM_AUTH" -H "Accept: application/json" ${TUTUM_REST_HOST}${service_link} | jq -r '.public_dns')
+        echo "${ip} ${host} ${fqdn}" >> /tmp/hosts
     done
 }
 
@@ -11,5 +11,5 @@ if [ -n "${TUTUM_AUTH}" ] && [ -n "${TUTUM_API_CALLS_FOR_DNS}" ]
 then
     curl -s -H "Authorization: $TUTUM_AUTH" -H "Accept: application/json" ${TUTUM_REST_HOST}/api/v1/container/ > /tmp/containers.raw
     cat /tmp/containers.raw | jq -r '.objects  | map ( "\(.private_ip) \(.name) \(.public_dns)" ) | .[]' | tr -d '"' >> /tmp/hosts
-    cat /tmp/containers.raw | jq -r '.objects  | map ( "\(.private_ip) \(.service)" ) | .[]' | tr -d '"' | parseServiceLinks
+    cat /tmp/containers.raw | jq -r '.objects  | map ( "\(.private_ip) \(.service) \(.name)" ) | .[]' | tr -d '"' | parseServiceLinks
 fi
