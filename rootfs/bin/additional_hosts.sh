dbc00eda121d65e777ea20bb75840a17644e8550
neilellis <hello@neilellis.me>
2015-09-04 01:32:28 +0100

    Additional hosts

--- rootfs/bin/additional_hosts.sh
+++ rootfs/bin/additional_hosts.sh
@@ -3,21 +3,18 @@ if [ -n "$BA_ADDITIONAL_HOSTS" ] && [ -n "$TUTUM_SERVICE_FQDN" ]
 then
     nameserver=$(cat /etc/dnsmasq-resolv.conf | grep nameserver | head -1 | cut -d' ' -f2)
     search=$(cat /etc/dnsmasq-resolv.conf | grep search | head -1 | cut -d' ' -f2)
-
     service1=$(echo $TUTUM_SERVICE_FQDN | cut -d'.' -f2-)
     service2=$(echo $TUTUM_SERVICE_FQDN | cut -d'.' -f3-)
     cont1=$(echo $TUTUM_CONTAINER_FQDN | cut -d'.' -f2-)
     cont2=$(echo $TUTUM_CONTAINER_FQDN | cut -d'.' -f3-)
 
-#$search $service1 $service2 $cont1 $cont2
-
     for host in $BA_ADDITIONAL_HOSTS
     do
-          for suffix in $search 
+          for suffix in $search $service1 $service2 $cont1 $cont2
           do
-              if ping -t 1 -c 1 "${host}.${suffix}"
+              if nslookup "${host}.${suffix}" ${nameserver}
               then
-                ip=$( nslookup "${host}.${suffix}" | grep Address | tail -1 | cut -d: -f2  | cut -d' ' -f2 2>/dev/null)
+                ip=$( nslookup "${host}.${suffix}" ${nameserver} | grep Address | tail -1 | cut -d: -f2  | cut -d' ' -f2 2>/dev/null)
                 echo "${ip} ${host}.${suffix}" >> /tmp/hosts
                 echo "Added additional host ${host}.${suffix}=${ip}"
                 break
